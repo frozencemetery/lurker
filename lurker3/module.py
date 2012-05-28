038becd990c175476beff4995ad1e7937ebe4546
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2012-05-27 20:51:06 -0400

    Created full python interface for modules.
    - moduleapi.org contains all modular APIs
    - module.py contains a python version of the above

---
+++ lurker3/module.py
@@ -0,0 +1,20 @@
+def cmdmsg(channel, channame, speaker, cmd):
+    return False
+def regmsg(channame, speaker, msg):
+    return
+def act(channame, speaker, msg):
+    return
+def userjoin(channame, joiner):
+    return
+def userpart(channame, parter):
+    return
+def userquit(channame, quitter):
+    return
+def botjoin(channame, botname):
+    return
+def botpart(channame, botname):
+    return
+def botpart(channame, botname):
+    return
+def botquit(botname):
+    return
