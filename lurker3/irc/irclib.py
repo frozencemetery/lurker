afd7003fca49761dcb85d327a2cbf57104604527
Colin Stanfill <colin.stanfill@gmail.com>
2012-05-30 16:38:33 -0400

    worked on irc lib

--- lurker3/irc/irclib.py
+++ lurker3/irc/irclib.py
@@ -8,13 +8,13 @@ class IrcError(Exception) :
         return repr(self.value)
 
 class IrcListener : # interface
-    def onPrivMsg(message, sender) :
+    def onPrivMsg(self, message, sender) :
         pass
 
-    def onChanMsg(message, sender, channel) :
+    def onChanMsg(self, message, sender, channel) :
         pass
 
-class IrcConnection :
+class IrcConnection(IrcListener) :
     
     def __init__(self, server, port, nick="lurker", realname="Helper P. Lurkington") :
         self.server = server
@@ -23,6 +23,9 @@ class IrcConnection :
         self.realname = realname
 
         self.initialize_listeners()
+
+    def initialize_listeners(self) :
+        self.OnMsg = []
 
     def connect(self) :
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,5 +36,18 @@ class IrcConnection :
         self.start_recv_loop()
 
     def start_recv_loop(self) :
+        line_buffer = ""
+        while True :
+            try :
+                line_buffer += self.socket.recv(256)
+            except Exception as e :
+                raise IrcError(e)
+            lines = line_buffer.split('\r\n')
+            for line in lines[:-1] :
+                self.interpret_line(line)
+            line_buffer = lines[-1]
 
-        
+    def interpret_line(self, line) :
+        pass
+
+
