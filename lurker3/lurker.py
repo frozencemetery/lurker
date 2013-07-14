0e41ee53170754f1d5ddbe4d7d2543300bdf74e3
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2013-07-14 15:51:04 -0400

    Merge test ("example") code into lurker.py, which now runs the bot.
    This also fixes a missing '+' error that wasn't caught before.

--- lurker3/lurker.py
+++ lurker3/lurker.py
@@ -3,6 +3,12 @@
 
 import sys
 
+import irc.irclib as irclib
+
+from irc.irclib import debug, warn, verbose, prnt
+from irc.irclib import BasicBehavior, SocketManager
+from irc.irclib import IrcConnection, IrcListener
+
 moddict = {}
 
 def load(modname):
@@ -37,7 +43,7 @@ for module in open(autoloadf, 'r').read():
     load(module)
     pass
   except:
-    print("module '" modname + "' failed to autoload")
+    print("module '" + modname + "' failed to autoload")
     pass
   pass
 
@@ -68,3 +74,47 @@ def onAct(channel, channame, speaker, msg):
                             channame, speaker, msg)
     pass
   pass
+
+class Lurker(IrcListener):
+  def __init__(self):
+    self.conn = IrcConnection("irc.foonetic.net", 6667,
+                              nick="lurker3", user="lurker3")
+    bb = BasicBehavior(["#lurkertest"])
+    self.conn.add_listener(bb)
+    self.conn.add_listener(self)
+    pass
+
+  def start(self):
+    self.conn.connect()
+    pass
+
+  def stop(self):
+    self.conn.disconnect()
+    pass
+
+  def on_chan_msg(self, owner, sender, channel, message):
+    owner.send(channel, message)
+    pass
+
+  def send(self, msg):
+    self.conn.send.privmsg("#lurkertest", msg)
+    pass
+  pass
+
+def main():
+  irclib.set_silent(False)
+  irclib.set_warn(True)
+  irclib.set_debug(True)
+  b = Lurker()
+  b.start()
+  s = ""
+  while s != "X":
+    s = raw_input()
+    b.send(s)
+    pass
+  b.stop()
+  SocketManager.exit()
+  pass
+
+if __name__ == "__main__":
+  main()
