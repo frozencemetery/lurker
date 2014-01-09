3e123efb48838bf3ac6002197fbc4a585a2c9b28
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2014-01-09 01:40:46 -0500

    Alerts module.
    No longer access disk every time something happens!

--- lurker3/modules/alerts.py
+++ lurker3/modules/alerts.py
@@ -1,21 +1,85 @@
 import time
-import cPickle
+
+import cPickle as P
 
 from module import *
 
-alertsdb = "rsrc/alerts.dict"
+alertdb = "modules/rsrc/alerts.db"
+alerts = {}
 
-lookupt = {}
+def loaddb():
+  global alerts
 
-f = open(alertsdb, 'r')
-lookupt = cPickle.load(f)
-f.close()
-
-def lookup(self, name):
   try:
-    val = lookupt[name]
-    return (True, val)
+    with open(alertdb, 'r') as f:
+      alerts = P.load(f)
+    pass
   except:
-    return (False, [])
+    print "failed to load alerts.db; corruption possible!"
+    pass
+  return
+
+def writedb():
+  global alerts
+
+  with open(alertdb, 'w') as f:
+    P.dump(alerts, f)
+    pass
+  return
+
+def maybe_alert(user):
+  global alerts
+
+  sentalert = False
+  try:
+    while alerts[user[0]] is not None:
+      user[-1](alerts[user[0]].pop())
+      sentalert = True
+      pass
+    pass
+  except:
+    pass
+  if sentalert:
+    writedb()
+    pass
+  return
+
+def cmdmsg(senderf, channame, speaker, cmdstr, isact):
+  maybe_alert(speaker)
+
+  if cmdstr.startswith("anonalert "):
+    cmdstr = cmdstr[4:] # delete "anon"
+    speaker = ["An anonymous user"]
+    pass
+
+  rem = speaker[0]
+  rem += (" (" + speaker[1] + speaker[2] + ")") if len(speaker) > 1 else ""
+  rem += time.strftime(" at %Y-%m-%d %H:%M:%S")
+  if cmdstr.startswith("alert "):
+    _, user, msg = cmdstr.split(" ", 2)
+    msg = rem + " told me to tell you " + msg
+    try:
+      alerts[user].append(msg)
+      pass
+    except:
+      alerts[user] = [msg]
+      pass
+    pass
+  return
+
+def regmsg(channame, speaker, cmdstr, isact):
+  return maybe_alert(speaker)
+
+def userjoin(channame, username):
+  return maybe_alert(speaker)
+
+def userpart(channame, username):
+  return maybe_alert(speaker)
+
+def unload():
+  writedb()
   pass
 
+######
+
+loaddb()
