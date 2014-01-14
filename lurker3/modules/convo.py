7c5384546b5036090b546b449dfa465fee469859
Colin Stanfill <colin.stanfill@gmail.com>
2014-01-14 14:41:21 -0500

    convogrep functionality

--- lurker3/modules/convo.py
+++ lurker3/modules/convo.py
@@ -1,10 +1,12 @@
 import random
+import re
 
 from module import *
 
 convodb = "modules/rsrc/convo.db"
 
 convos = [] # globalize THIS
+lastgrep = {} # remaining convos, associated by location
 
 def loaddb():
   global convos
@@ -27,6 +29,23 @@ def getconvo():
   global convos
 
   return random.choice(convos)
+
+def grepconvos(channel, regex):
+  global convos
+  global lastgrep
+  matching = filter((lambda line: re.match(".*" + regex + ".*", line, re.IGNORECASE)), convos)
+  lastgrep[channel] = matching
+  return nextgrep(channel)
+
+def nextgrep(channel) :
+  global lastgrep
+
+  if not channel in lastgrep.keys() :
+    return "No extant queries"
+  if len(lastgrep[channel]) == 0 :
+    return "No (more) matches found."
+  fst, lastgrep[channel] = lastgrep[channel][0], lastgrep[channel][1:]
+  return fst + (" {1}[+{0}]{1}".format(len(lastgrep[channel]), chr(2)))
 
 def writedb():
   with open(convodb, 'w') as f:
@@ -57,7 +76,11 @@ def cmdmsg(senderf, channel, speaker, cmd, isact):
     addconvo(cmd.split(" ", 2)[2])
     senderf("NOW WE'RE HAVING A GOOD TIME RIGHT")
     return True
-
+  elif cmd.startswith("convo grep "):
+    senderf(grepconvos(channel, cmd.split(" ", 2)[2]))
+    return True
+  elif cmd == "convo next":
+    senderf(nextgrep(channel))
   return False
 
 def unload():
