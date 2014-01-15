3eaef62675888d122513a398937561c07bd04941
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2014-01-15 12:05:07 -0500

    Hugs module with nohug support.
    Not mainlining until actions are working properly.

---
+++ lurker3/modules/hug.py
@@ -0,0 +1,62 @@
+import cPickle as P
+
+from module import *
+
+unhugged = []
+
+nohugs_file = "modules/rsrc/nohugs.db"
+
+def loaddb():
+  global unhugged
+
+  try:
+    with open(nohugs_file, 'r') as f:
+      unhugged = P.load(f)
+      pass
+    pass
+  except:
+    print "Failed to load nohugs.db; corruption possible!"
+    pass
+  return
+
+def writedb():
+  global unhugged
+
+  try:
+    with open(nohugs_file, 'w') as f:
+      P.dump(unhugged, f)
+      pass
+    pass
+  except:
+    print "Failed to dump nohugs.db; hugs for everyone!"
+    pass
+  return
+
+def cmdmsg(senderf, channel, speaker, cmd, isact):
+  global unhugged
+
+  if cmd == "hug me never" and speaker[0] not in unhugged:
+    unhugged.append(speaker[0])
+    writedb()
+    senderf(speaker[0] + ": I will remember that")
+  elif cmd.startswith("hug "):
+    name = cmd.split(" ", 1)[1]
+    if name == "me":
+      name = speaker[0]
+      pass
+    if name not in unhugged:
+      senderf("hugs " + name, isact=True)
+      pass
+    else:
+      senderf("refuses to violate " + name, isact=True)
+      pass
+    return True
+  return False
+
+def unload():
+  writedb()
+  return
+
+######
+
+loaddb()
