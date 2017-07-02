69fef953419cf8cfd6fbdc558de5e5190d33d09e
Robbie Harwood <rharwood@club.cc.cmu.edu>
2017-07-01 21:19:59 -0400

    Modernize fml

--- modules/fml.py
+++ modules/fml.py
@@ -6,31 +6,31 @@ from module import *
 fmlqueue = []
 
 def obtain():
-  global fmlqueue
+    global fmlqueue
 
-  html = urllib2.urlopen("http://www.fmylife.com/random").read()
-  html = re.findall("Today.*?FML", html)
-  del(html[0]) # delete how to make an FML
-  html = [re.sub("\<.*?>", "", x) for x in html]
-  fmlqueue += html
-  return
+    html = urllib2.urlopen("http://www.fmylife.com/random").read()
+    html = re.findall("Today.*?FML", html)
+    del(html[0]) # delete how to make an FML
+    html = [re.sub("\<.*?>", "", x) for x in html]
+    fmlqueue += html
+    return
 
 def cmdmsg(senderf, channel, speaker, cmd, isact):
-  global fmlqueue
+    global fmlqueue
 
-  if cmd != "fml":
-    return False
+    if cmd != "fml":
+        return False
 
-  if len(fmlqueue) <= 0:
-    try:
-      obtain()
-      pass
-    except:
-      senderf("Could not load from FML and cache empty!")
-      return True
-    pass
-  senderf(fmlqueue.pop())
-  return True
+    if len(fmlqueue) <= 0:
+        try:
+            obtain()
+            pass
+        except:
+            senderf("Could not load from FML and cache empty!")
+            return True
+        pass
+    senderf(fmlqueue.pop())
+    return True
 
 ######
 
